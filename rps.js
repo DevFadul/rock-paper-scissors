5dab179a50b8bc54a2cbf3ebfb54813f6f334ef0
DevFadul <bruhbruhfolife@gmail.com>
2023-08-07 22:45:09 +0300

    create all functions and test run the game

--- rps.js
+++ rps.js
@@ -1,10 +1,45 @@
 function getComputerChoice() {
-    const choice = ["Rock","Paper","Scissors"];
+    const choice = ["rock","paper","scissors"];
     return choice[Math.floor(Math.random()*choice.length)]
 }
 
-function playRound (playerSelection,computerSelection) {
-    return "You Lose! Paper beats Rock";
+function checkWinner (playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "Tie"
+    }
+    else if (
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ) {
+        return "Player"
+    }
+    else {
+        return "Computer"
+    }
 }
 
-console.log(playRound());
+function playRound (playerSelection,computerSelection) {
+    const result = checkWinner(playerSelection,computerSelection)
+    if (result == "Tie") {
+        return "its a tie!"
+    }
+    else if (result == "Player") {
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    }
+    else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+function game() {
+console.log("Welcome")
+for (let i = 0; i<5;i++){
+    const playerSelection = prompt("rock paper or scissors?");
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection,computerSelection));
+}
+}
+
+game();
+
